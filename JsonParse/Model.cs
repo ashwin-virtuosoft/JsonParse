de2206593b88ce202ef5d8e3175295257fd017a2
ashwin-virtuosoft <ashwin.kumar@virtuosoft.in>
2024-04-03 17:39:22 +0530

    Add project files.

---
+++ JsonParse/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonParse
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public int Age {  get; set; }
+        public int RollNo {  get; set; }
+    }
+}
---
+++ JsonParse/Program.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace JsonParse
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ParseObjectToJson();
+        }
+
+        private static void ParseObjectToJson()
+        {
+            Model model = new Model
+            {
+                Id = 1,
+                Age = 22,
+                RollNo = 23,
+            };
+
+
+
+            string jsonString = JsonConvert.SerializeObject(model);
+            DeserializeJsonToObject(jsonString);
+            Console.WriteLine(jsonString);
+            Console.ReadLine();
+        }
+
+        private static void DeserializeJsonToObject(string jsonString)
+        {
+            Model model = JsonConvert.DeserializeObject<Model>(jsonString);
+        }
+            private static void ParseXmlObject()
+        {
+            var model = new Model()
+            {
+                Id = 1,
+                Age = 22,
+                RollNo = 23,
+            };
+
+            var xmlSerializer = new XmlSerializer(typeof(Model));
+            using (var writer = new StringWriter())
+            {
+                xmlSerializer.Serialize(writer, model);
+                var xmlContent = writer.ToString();
+                DesirializeXmltoObject(xmlContent);
+                Console.WriteLine(xmlContent);
+                Console.ReadKey();
+
+
+            }
+
+        }
+
+        private static void DesirializeXmltoObject(string xmlString)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Model));
+            using (var reader = new StringReader(xmlString))
+            {
+                var model = (Model)xmlSerializer.Deserialize(reader);
+            }
+        }
+
+    }
+}
+
